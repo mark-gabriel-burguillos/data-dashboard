2d91ffb0b22fb9cf56819e77bf88e7fb3e314abb
programmernammer <programmernammer@gmail.com>
2021-12-02 01:52:45 +0800

    Fitted embedded dashboard to take the remaining screen. Removed the sidebar's opening and closing function.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 
 import NavigationBar from "../NavigationBar/NavigationBar";
-import SideBarClosed from "../SideBarClosed/SideBarClosed";
-import SideBarOpen from "../SideBarOpen/SideBarOpen";
+import SideBar from "../SideBar/SideBar";
 import styles from "./App.module.css";
 
 import "./App.css";
 import Tableau from "../Tableau";
 
 const App = () => {
-  const [isSideBarOpen, handleSideBarOpen] = useState(true);
-
   return (
     <div className={styles.App}>
       <NavigationBar />
       <div className={styles.Content}>
-        {isSideBarOpen ? (
-          <SideBarOpen handleOnClickEvent={handleSideBarOpen} />
-        ) : (
-          <SideBarClosed handleOnClickEvent={handleSideBarOpen} />
-        )}
+        <SideBar />
+        <div className={styles.Content__Dashboard}>
+          <Tableau />
+        </div>
       </div>
-      <Tableau />
     </div>
   );
 };
