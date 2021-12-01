8dcbbd02f461699efe2440e78587226fb161ee26
programmernammer <programmernammer@gmail.com>
2021-12-02 03:28:57 +0800

    Set Tableau API argument to parameter 'device' to 'desktop' to follow intended layout

--- src/Components/Tableau.js
+++ src/Components/Tableau.js
@@ -5,7 +5,7 @@ const { tableau } = window;
 const Tableau = () => {
   const ref = useRef(null);
   const url =
-    "https://public.tableau.com/views/Book1_16383773963890/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link";
+    "https://public.tableau.com/views/Book1_16383773963890/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link&:device=desktop";
 
   function initViz() {
     new tableau.Viz(ref.current, url);
