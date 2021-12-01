f84dc5bf4db1bcc7d0c0409b76ff89bf7ac5fd21
programmernammer <programmernammer@gmail.com>
2021-12-02 01:55:27 +0800

    Renamed SideBarOpen component to SideBar

---
+++ src/Components/SideBar/SideBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./SideBar.module.css";
+
+const SideBar = () => {
+  return (
+    <aside className={styles.SideBar}>
+      <div className={styles.SideBar}></div>
+    </aside>
+  );
+};
+
+export default SideBar;
