426fb16a6bb01bd92c4e5f76fd26d28d3836035c
programmernammer <programmernammer@gmail.com>
2021-12-06 17:48:25 +0800

    Finalized

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,35 +10,35 @@ import Tableau from "../Tableau";
 const tableauTables = [
   {
     TabName: "Top X directors by number of movies produced",
-    Link: "https://public.tableau.com/views/Official_16387492955330/TopDirectors?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/TopDirectors?:origin=card_share_link&:embed=n",
   },
   {
     TabName: "Top X movie genres by number of movies",
-    Link: "https://public.tableau.com/views/Official_16387492955330/TopGenres?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/TopGenres?:origin=card_share_link&:embed=n",
   },
   {
-    TabName: "Actor History",
-    Link: "https://public.tableau.com/shared/74JKNMJK2?:display_count=n&:origin=viz_share_link",
+    TabName: "Actor Dashboard",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/ActorDashboard?:origin=card_share_link&:embed=n",
   },
   {
-    TabName: "Director History",
-    Link: "https://public.tableau.com/views/Official_16387492955330/DirectorDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    TabName: "Director Dashboard",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/DirectorDashboard?:origin=card_share_link&:embed=n",
   },
   {
     TabName: "Number of Movies per Director per Genre",
-    Link: "https://public.tableau.com/views/Official_16387492955330/NumberofMoviesperDirectorperGenre?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/NumberofMoviesperDirectorperGenre?:origin=card_share_link&:embed=n",
   },
   {
     TabName: "Number of Actors per Movie Genre per Director ID",
-    Link: "https://public.tableau.com/views/Official_16387492955330/NumberofActorsperMovieGenreperDirectorID?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/NumberofActorsperMovieGenreperDirectorID?:origin=card_share_link&:embed=n",
   },
   {
     TabName: "Number of Movies per Genre in the Year X",
-    Link: "https://public.tableau.com/views/Official_16387492955330/NumberofMoviesperGenreintheYear?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/NumberofMoviesperGenreintheYear?:origin=card_share_link&:embed=n",
   },
   {
     TabName: "Number of Movies of Genre W and X in years Y or Z",
-    Link: "https://public.tableau.com/views/Official_16387492955330/Dice?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    Link: "https://prod-apnortheast-a.online.tableau.com/t/imdbdatahost/views/Official/Dice?:origin=card_share_link&:embed=n",
   },
 ];
 
